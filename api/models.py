edaa0617ef1f719681c56cbad183df5a15500e2a
andyi95 <linuxoid.andrew@gmail.com>
2021-08-17 12:51:14 +0300

    Initial commit

---
+++ api/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Currency(models.Model):
+    """
+    A separate class for base currencies info storage
+    """
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Название валюты (rus)'
+    )
+    eng_name = models.CharField(
+        max_length=255,
+        verbose_name='Название валюты (eng)'
+    )
+    nominal = models.PositiveSmallIntegerField(
+        verbose_name='Номинал единицы'
+    )
+    parent_code = models.CharField(
+        max_length=255,
+        verbose_name='Parent code'
+    )
+    iso_num = models.PositiveSmallIntegerField(
+        verbose_name='Числовой ISO код'
+    )
+    iso_char = models.CharField(
+        max_length=255,
+        verbose_name='Символьный ISO код'
+    )
